5498023ada0d9d128f0c7de76a2ff786adb783f1
ferenc_koler <ferenckoler@outlook.com>
2024-05-10 21:15:02 +0200

    Homework #1 Update #1

--- BEP/homework_1/src/main/java/com/iktobuka/homework_1/controllers/BankClientRestController.java
+++ BEP/homework_1/src/main/java/com/iktobuka/homework_1/controllers/BankClientRestController.java
@@ -1,5 +1,7 @@
 package com.iktobuka.homework_1.controllers;
 
+import java.time.LocalDate;
+import java.time.Period;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -19,21 +21,28 @@ public class BankClientRestController {
 	private List<BankClientEntity> getDB() {
 		List<BankClientEntity> clients = new ArrayList<>();
 
-		clients.add(new BankClientEntity(1, "Albert", "Einstein", "albert@einstein.io"));
-		clients.add(new BankClientEntity(2, "Marie", "Curie", "marie@curie.io"));
-		clients.add(new BankClientEntity(3, "Isaac", "Newton", "isaac@newton.io"));
-		clients.add(new BankClientEntity(4, "Charles", "Darwin", "charles@darwin.io"));
-		clients.add(new BankClientEntity(5, "Galileo", "Galilei", "galileo@galilei.io"));
-		clients.add(new BankClientEntity(6, "Nikola", "Tesla", "nikola@tesla.io"));
-		clients.add(new BankClientEntity(7, "Stephen", "Hawking", "stephen@hawking.io"));
-		clients.add(new BankClientEntity(8, "Ada", "Lovelace", "ada@lovelace.io"));
-		clients.add(new BankClientEntity(9, "Leonardo", "da Vinci", "leonardo@davinci.io"));
-		clients.add(new BankClientEntity(10, "Carl", "Sagan", "carl@sagan.io"));
+		clients.add(
+				new BankClientEntity(1, "Barack", "Obama", "barack@obama.io", LocalDate.of(1961, 8, 4), "Washington"));
+		clients.add(
+				new BankClientEntity(2, "Angela", "Merkel", "angela@merkel.io", LocalDate.of(1954, 7, 17), "Berlin"));
+		clients.add(new BankClientEntity(3, "Elon", "Musk", "elon@musk.io", LocalDate.of(1971, 6, 28), "Los_Angeles"));
+		clients.add(
+				new BankClientEntity(4, "Oprah", "Winfrey", "oprah@winfrey.io", LocalDate.of(1954, 1, 29), "Chicago"));
+		clients.add(new BankClientEntity(5, "Jeff", "Bezos", "jeff@bezos.io", LocalDate.of(1964, 1, 12), "Seattle"));
+		clients.add(new BankClientEntity(6, "Bill", "Gates", "bill@gates.io", LocalDate.of(1955, 10, 28), "Seattle"));
+		clients.add(
+				new BankClientEntity(7, "Warren", "Buffett", "warren@buffett.io", LocalDate.of(1930, 8, 30), "Omaha"));
+		clients.add(new BankClientEntity(8, "Larry", "Page", "larry@page.io", LocalDate.of(1973, 3, 26), "Palo_Alto"));
+		clients.add(new BankClientEntity(9, "Mark", "Cuban", "mark@cuban.io", LocalDate.of(1958, 7, 31), "Dallas"));
+		clients.add(new BankClientEntity(10, "Richard", "Branson", "richard@branson.io", LocalDate.of(1950, 7, 18),
+				"London"));
 
 		return clients;
 	}
 
-//	===============================
+	private List<BankClientEntity> clients = getDB();
+
+//	@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 
 	@RequestMapping(method = RequestMethod.GET, value = "/emails")
 	public List<String> getEmails() {
@@ -50,7 +59,6 @@ public class BankClientRestController {
 	@RequestMapping(method = RequestMethod.GET, value = "/clients/{firstLetter}")
 	public List<String> getClientsByFirstLetter(@PathVariable char firstLetter) {
 		List<String> matchingNames = new ArrayList<>();
-		List<BankClientEntity> clients = getDB();
 
 		for (BankClientEntity bc : clients) {
 			if (bc.getName().toLowerCase().charAt(0) == Character.toLowerCase(firstLetter)) {
@@ -65,7 +73,6 @@ public class BankClientRestController {
 	public List<String> getClientsByFirstLetters(@RequestParam("firstLetterName") char firstLetterName,
 			@RequestParam("firstLetterSurname") char firstLetterSurname) {
 		List<String> matchingNames = new ArrayList<>();
-		List<BankClientEntity> clients = getDB();
 
 		char upperCaseName = Character.toUpperCase(firstLetterName);
 		char upperCaseSurname = Character.toUpperCase(firstLetterSurname);
@@ -85,7 +92,6 @@ public class BankClientRestController {
 	@RequestMapping(method = RequestMethod.GET, value = "/clients/sort/{order}")
 	public List<String> sortClients(@PathVariable String order) {
 		List<String> sortedNames = new ArrayList<>();
-		List<BankClientEntity> clients = getDB();
 
 		for (BankClientEntity bc : clients) {
 			sortedNames.add(bc.getName());
@@ -102,5 +108,111 @@ public class BankClientRestController {
 		return sortedNames;
 	}
 
-//	===============================
+//	@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+
+	private int calculateAge(LocalDate dateOfBirth) {
+		LocalDate now = LocalDate.now();
+		return Period.between(dateOfBirth, now).getYears();
+	}
+
+	@RequestMapping(method = RequestMethod.PUT, value = "/clients/bonitet")
+	public List<BankClientEntity> setCreditRatings() {
+
+		for (BankClientEntity client : clients) {
+			LocalDate dob = client.getDateOfBirth();
+			char creditRating = 'N';
+
+			if (calculateAge(dob) < 65) {
+				creditRating = 'P';
+			}
+
+			client.setCreditRating(creditRating);
+		}
+
+		return clients;
+	}
+
+	@RequestMapping(method = RequestMethod.DELETE, value = "/clients/delete")
+	public String deleteClient() {
+		List<BankClientEntity> validClients = new ArrayList<>();
+
+		for (BankClientEntity client : clients) {
+			if (!client.getName().isEmpty() && !client.getSurname().isEmpty() && !client.getEmail().isEmpty()) {
+				validClients.add(client);
+			}
+		}
+
+		clients = validClients;
+		return "Clients without required information have been successfully removed.";
+	}
+
+	@RequestMapping(method = RequestMethod.GET, value = "/clients/countLess/{years}")
+	public List<BankClientEntity> youngerThan(@PathVariable int years) {
+		List<BankClientEntity> result = new ArrayList<>();
+
+		for (BankClientEntity client : clients) {
+			if (calculateAge(client.getDateOfBirth()) < years) {
+				result.add(client);
+			}
+		}
+
+		return result;
+	}
+
+	@RequestMapping(method = RequestMethod.GET, value = "/clients/averageYears")
+	public double averageClientAge() {
+		int totalAge = 0;
+
+		for (BankClientEntity client : clients) {
+			totalAge += calculateAge(client.getDateOfBirth());
+		}
+
+		return (double) totalAge / clients.size();
+	}
+
+//	@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+
+	@RequestMapping(method = RequestMethod.PUT, value = "/clients/changelocation/{clientId}")
+	public String changeLocation(@PathVariable int clientId, @RequestParam("city") String city) {
+
+		for (BankClientEntity client : clients) {
+			if (client.getId() == clientId) {
+				String currentCity = client.getCity();
+				client.setCity(city);
+
+				return "Residence of the client " + client.getName() + " " + client.getSurname() + " from "
+						+ currentCity + " successfully changed to " + city;
+			}
+		}
+
+		return "Client with ID " + clientId + " not found.";
+	}
+
+	@RequestMapping(method = RequestMethod.GET, value = "/clients/from/{city}")
+	public List<BankClientEntity> getClientsFromCity(@PathVariable String city) {
+		List<BankClientEntity> clientsFromCity = new ArrayList<>();
+
+		for (BankClientEntity client : clients) {
+			if (client.getCity() != null && client.getCity().equalsIgnoreCase(city)) {
+				clientsFromCity.add(client);
+			}
+		}
+
+		return clientsFromCity;
+	}
+
+	@RequestMapping(method = RequestMethod.GET, value = "/clients/findByCityAndAge")
+	public List<BankClientEntity> getClientsByCityAndAge(@RequestParam("city") String city,
+			@RequestParam("age") int age) {
+		List<BankClientEntity> matchingClients = new ArrayList<>();
+
+		for (BankClientEntity client : clients) {
+			if (client.getCity() != null && client.getCity().equalsIgnoreCase(city)
+					&& calculateAge(client.getDateOfBirth()) < age) {
+				matchingClients.add(client);
+			}
+		}
+
+		return matchingClients;
+	}
 }
--- BEP/homework_1/src/main/java/com/iktobuka/homework_1/entities/BankClientEntity.java
+++ BEP/homework_1/src/main/java/com/iktobuka/homework_1/entities/BankClientEntity.java
@@ -1,14 +1,19 @@
 package com.iktobuka.homework_1.entities;
 
+import java.time.LocalDate;
+
 public class BankClientEntity {
 	protected Integer id;
 	protected String name;
 	protected String surname;
 	protected String email;
-
+	protected LocalDate dateOfBirth;
+    protected char creditRating;
+    protected String city;
+	
 	public Integer getId() {
 		return id;
-	}
+	}	
 
 	public void setId(Integer id) {
 		this.id = id;
@@ -37,18 +42,99 @@ public class BankClientEntity {
 	public void setEmail(String email) {
 		this.email = email;
 	}
+	
+	public LocalDate getDateOfBirth() {
+		return dateOfBirth;
+	}
+
+	public void setDateOfBirth(LocalDate dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}
+
+	public char getCreditRating() {
+		return creditRating;
+	}
+
+	public void setCreditRating(char creditRating) {
+		this.creditRating = creditRating;
+	}
+	
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
 
 	public BankClientEntity() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
 
-	public BankClientEntity(Integer id, String name, String surname, String email) {
+	public BankClientEntity(
+			Integer id, 
+			String name, 
+			String surname, 
+			String email
+		) {
 		super();
 
 		this.setId(id);
 		this.setName(name);
 		this.setSurname(surname);
 		this.setEmail(email);
+	}
+	
+	public BankClientEntity(
+			Integer id, 
+			String name, 
+			String surname, 
+			String email, 
+			LocalDate dateOfBirth
+		) {
+		super();
+
+		this.setId(id);
+		this.setName(name);
+		this.setSurname(surname);
+		this.setEmail(email);
+		this.setDateOfBirth(dateOfBirth);
+	}
+	
+	public BankClientEntity(
+			Integer id, 
+			String name, 
+			String surname, 
+			String email, 
+			LocalDate dateOfBirth, 
+			char creditRating
+		) {
+		super();
+
+		this.setId(id);
+		this.setName(name);
+		this.setSurname(surname);
+		this.setEmail(email);
+		this.setDateOfBirth(dateOfBirth);
+		this.setCreditRating(creditRating);
+	}
+	
+	public BankClientEntity(
+			Integer id, 
+			String name, 
+			String surname, 
+			String email, 
+			LocalDate dateOfBirth,
+			String city
+		) {
+		super();
+
+		this.setId(id);
+		this.setName(name);
+		this.setSurname(surname);
+		this.setEmail(email);
+		this.setDateOfBirth(dateOfBirth);
+		this.setCity(city);
 	}
 }
